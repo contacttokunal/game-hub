502bd13037c7d71b7051604ee9b6237aa986b95b
kunal <contacttokunal@gmail.com>
2023-07-24 02:55:15 +0530

    Refactoring remove duplicate interface

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,7 +1,7 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
+import { Platform } from "../hooks/usePlatforms";
 import usePlatforms from "../hooks/usePlatforms";
-import { Platform } from "../hooks/useGames";
 
 interface Props {
   onSelectePlatform: (platform: Platform) => void;
